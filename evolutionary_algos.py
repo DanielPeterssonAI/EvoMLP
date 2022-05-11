8e222169a23a7c70627443bdfaca89536c6357dd
DanielPeterssonAI <89392459+DanielPeterssonAI@users.noreply.github.com>
2022-05-11 21:52:53 +0200

    regressor changes

--- evolutionary_algos.py
+++ evolutionary_algos.py
@@ -2,11 +2,16 @@ import numpy as np
 import math
 
 class EvoMLPRegressor:
-
-    '''THIS IS THE ONE TO USE'''
-
-    def __init__(self, n = 24, hidden_layers = False, activation = "relu",  lr_target = 0.04, lr_initial_decay = 60, lr_final_decay = 0.03, random_state = None):
-
+    def __init__(
+        self, 
+        n = 24, 
+        hidden_layer_sizes = False, 
+        activation = "relu", 
+        lr_target = 0.002, 
+        lr_initial_decay = 20, 
+        lr_final_decay = 0.02, 
+        random_state = None
+    ):
         self.n = int(round(n / 8) * 8)
         self.validation_loss_history = []
         self.training_loss_history = []
@@ -15,11 +20,7 @@ class EvoMLPRegressor:
         self.lr_target = lr_target
         self.lr_initial_decay = lr_initial_decay
         self.lr_final_decay = lr_final_decay
-        
-        if hidden_layers:
-            self.layers = hidden_layers + [1]
-        else:
-            self.layers = [1]
+        self.hidden_layer_sizes = hidden_layer_sizes
 
     def fit(self, X_train, y_train, epochs = 100, validation_data = False, verbose = 0):
 
@@ -35,27 +36,28 @@ class EvoMLPRegressor:
             activation_function = lambda x: np.maximum(0.1 * x, x)
         elif self.activation == "relu":
             activation_function = lambda x: np.maximum(0, x)
-
+        
+        # Add bias column to X
         X_train = np.c_[np.ones(X_train.shape[0]), X_train]
-
+        
+        if self.hidden_layer_sizes:
+            layers = [X_train.shape[1]] + self.hidden_layer_sizes + [1]
+        else:
+            layers = [X_train.shape[1]] + [1]
+        
         n = self.n
         ndiv4 = n // 4
+        number_of_layers_minus_one = len(layers) - 1
 
         lr_target = self.lr_target
         lr_initial_decay = self.lr_initial_decay
         lr_final_decay = self.lr_final_decay
-
-        layers = [X_train.shape[1]] + self.layers
-
-        number_of_layers_minus_one = len(layers) - 1
-
+        
         y_preds = np.zeros((n, y_train.shape[0]))
-
         nets_loss = np.zeros(n)
         sorted_indices = np.arange(-(ndiv4), n, 1)
-        
+
         best_net_index = -1
-        
         weights = []
 
         for i in range(number_of_layers_minus_one):
@@ -64,19 +66,20 @@ class EvoMLPRegressor:
         for epoch in range(epochs):
             forward_pass = X_train.T
             
+            # Hidden layers
             for j in range(number_of_layers_minus_one - 1):
                 forward_pass = activation_function(weights[j][sorted_indices[ndiv4:]].transpose(0, 2, 1) @ forward_pass)
 
+            # Output layer
             forward_pass = weights[-1][sorted_indices[ndiv4:]].transpose(0, 2, 1) @ forward_pass
             
+            # Fill in in the predictions from the new nets
             y_preds[sorted_indices[ndiv4:]] = forward_pass.reshape(*forward_pass.shape[::2])
-
             nets_loss[sorted_indices[ndiv4:]] = np.mean(np.abs(y_preds[sorted_indices[ndiv4:]] - y_train), axis = 1)
-
+            
             sorted_indices = np.argsort(nets_loss)
 
-            mutation_sigma = math.exp(-epoch / (epochs / (lr_initial_decay * math.log10(epochs + 1)))) + lr_final_decay * math.exp(-(epoch + 1) * (1 / (epochs))) + lr_target + (-0.036 * 10 * lr_final_decay)
-            #mutation_sigma = math.exp(-epoch / (epochs / (lr_decay * math.log10(epochs + 1)))) + 0.02 * math.exp(-(epoch + 1) * (1 / (epochs + 1))) - 0.005
+            mutation_sigma = math.exp(-epoch / (epochs / (lr_initial_decay * math.log10(epochs + 1)))) + lr_final_decay * math.exp(-(epoch + 1) * (1 / (epochs))) + lr_target + (-0.035 * 10 * lr_final_decay)
 
             for j in range(number_of_layers_minus_one):
                 weights[j][sorted_indices[0 + ndiv4::6]] = (weights[j][sorted_indices[0: ndiv4: 2]] + weights[j][sorted_indices[1: ndiv4: 2]]) / 2 + np.random.normal(0, mutation_sigma, (ndiv4 // 2, layers[j], layers[j + 1]))
@@ -97,11 +100,11 @@ class EvoMLPRegressor:
                 if validation_data:
                     self.validation_loss_history += [np.mean(np.abs(y_val - self.predict(X_val)))]
                     if verbose == 1:
-                        print(f"Epoch {epoch} - loss: {self.training_loss_history[-1]} - val_loss: {self.validation_loss_history[-1]}")
+                        print(f"Epoch {epoch} - loss: {self.training_loss_history[-1]} - val_loss: {self.validation_loss_history[-1]} - sigma: {mutation_sigma}")
                 else:
                     if verbose == 1:
                         pass
-                        print(f"Epoch {epoch} - loss: {self.training_loss_history[-1]}")
+                        print(f"Epoch {epoch} - loss: {self.training_loss_history[-1]} - sigma: {mutation_sigma}")
 
 
     def predict(self, X):
